dd823a7aa5bc71fceebef0405dc6a0685f5d9185
Santiago José Dandois <santiago.dandois@widergy.com>
2022-06-07 21:09:56 -0300

    reproduce bug

---
+++ App.js
@@ -0,0 +1,159 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React, {useEffect} from 'react';
+import type {Node} from 'react';
+import Realm from 'realm';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+class TestObject extends Realm.Object {
+  static generate() {
+    return {
+      primaryKey: 'current',
+      dictionary: {},
+    };
+  }
+
+  static schema = {
+    name: 'User',
+    primaryKey: 'primaryKey',
+    properties: {
+      primaryKey: 'string',
+      dictionary: '{}',
+    },
+  };
+}
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  useEffect(() => {
+    const init = async () => {
+      console.log('init');
+      const realm = await Realm.open({
+        schema: [TestObject],
+      });
+
+      console.log('opened');
+
+      realm.write(() => {
+        realm.create(TestObject, TestObject.generate(), 'all');
+      });
+
+      const object = realm.objectForPrimaryKey(TestObject, 'current');
+
+      console.log('before some');
+
+      /**
+       * The following line is the problematic one. It hangs for array like functions.
+       */
+      object.dictionary.forEach();
+      console.log('after some (never executes)');
+    };
+
+    init().catch(err => {
+      console.error('Err', err);
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
